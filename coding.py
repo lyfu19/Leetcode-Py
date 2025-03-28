859f70784894245bf42dd0cdb2ff97e87d9611df
Yongfu Lin <alfielin0709@gmail.com>
2025-03-29 10:38:04 +1300

    1422. Maximum Score After Splitting a String

--- coding.py
+++ coding.py
@@ -2779,6 +2779,21 @@ class Solution:
                 ans.append(digits.pop())
         return ''.join(ans)
 
-s = "a0b1c2"
-result = Solution().reformat(s)
+    def maxScore(self, s: str) -> int:
+        left = 0
+        right = s.count('1')
+
+        max_score = 0
+        for i in range(len(s) - 1):
+            if s[i] == '0':
+                left += 1
+            else:
+                right -= 1
+            max_score = max(max_score, left + right)
+        return max_score
+
+
+
+s = "1111"
+result = Solution().maxScore(s)
 print(result)
