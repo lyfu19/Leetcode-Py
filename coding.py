7cc0a691bbd7e2f370ac3c4efd9e502ddc1acd18
Yongfu Lin <alfielin0709@gmail.com>
2025-05-09 13:20:26 +1200

    1550. Three Consecutive Odds

--- coding.py
+++ coding.py
@@ -2997,7 +2997,17 @@ class Solution:
                 stack.append(ch)
         return ''.join(stack)
 
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        count = 0
+        for n in arr:
+            if n % 2 == 1:
+                count += 1
+                if count >= 3:
+                    return True
+            else:
+                count = 0
+        return False
 
-s = "s"
-result = Solution().makeGood(s)
+arr = [1,2,34,3,4,5,7,23,12]
+result = Solution().threeConsecutiveOdds(arr)
 print(result)
